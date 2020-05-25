3ca618984c9f40b495129db44684a1d1feb66c93
Hugh Bromund <hughbromund44@gmail.com>
2020-05-24 20:34:56 -0400

    Updates

--- v2/src/components/NotFound/NotFound.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-class NotFound extends Component {
-    render() {
-      return (<div>404: Page Not Found</div>)
-    }
-}
-export default NotFound;
--- v2/src/constants/Constants.js
+++
@@ -1,5 +0,0 @@
-export const HOME_PATH = "/";
-export const ABOUT_ME = "/about";
-export const PROJECTS = "/projects";
-
-export const TEXT_COLOR = "#58b4ae";
